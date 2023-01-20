27399eca8ebd76fe9793c895867a4f4d528c4f20
alisam163 <alisam163@yandex.ru>
2023-01-20 12:50:39 +0300

    20/01

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
 import './App.css'
 import React, { Component } from 'react'
 import Header from './components/Header/Header'
-import RandomPlanet from './components/RandomPlanet/RandomPlanet'
 import SwapiService from './services/SwapiService'
-import PersonPage from './components/PersonPage/PersonPage'
 import ErrorIndicator from './components/ErrorIndicator/ErrorIndicator'
-import Row from './components/UI/Row/Row'
-import ErrorBoundry from './components/ErrorBoundry/ErrorBoundry'
-import ItemDetails from './components/ItemDetails/ItemDetails'
+import PersonPage from './components/PersonPage/PersonPage'
 
 class App extends Component {
   swapiService = new SwapiService()
@@ -26,25 +22,7 @@ class App extends Component {
   }
 
   render() {
-    const { getPerson, getStarships, getPersonImage, getStarshipsImage } =
-      this.swapiService
-
-    const personDetails = (
-      <ItemDetails
-        itemId={11}
-        getData={getPerson}
-        getImageUrl={getPersonImage}
-      />
-    )
-
-    const StarshipDetails = (
-      <ItemDetails
-        itemId={5}
-        getData={getStarships}
-        getImageUrl={getStarshipsImage}
-      />
-    )
-
+    const { getAllPerson } = this.swapiService
     if (this.state.hasError) {
       return (
         <>
@@ -53,14 +31,10 @@ class App extends Component {
       )
     }
     return (
-      <ErrorBoundry>
-        <div className="App">
-          <Header />
-          {/* <RandomPlanet />
-        <PersonPage getData={this.swapiService.getAllPeople} /> */}
-          <Row left={personDetails} right={StarshipDetails} />
-        </div>
-      </ErrorBoundry>
+      <div className="App">
+        <Header />
+        <PersonPage getData={getAllPerson} />
+      </div>
     )
   }
 }
